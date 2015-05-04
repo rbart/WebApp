f42106c64623550ebbc0cf9952a9e0894a70245f
Rob Bart <rbart234@gmail.com>
2015-05-03 19:06:58 -0700

    Added basic MongoDB connection.

--- src/main/java/helloworld/HelloWorldServlet.java
+++ src/main/java/helloworld/HelloWorldServlet.java
@@ -1,8 +1,16 @@
 package helloworld;
 
 import javax.servlet.*;
+
 import java.io.*;
+
 import javax.servlet.http.*;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @SuppressWarnings("serial")
 public class HelloWorldServlet extends HttpServlet {
@@ -11,9 +19,20 @@ public class HelloWorldServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+		MongoDatabase database = mongoClient.getDatabase("test");
+		
+		MongoCollection<Document> collection = database.getCollection("restaurants");
+
 		PrintWriter out = res.getWriter();
 
-		out.println("Hello, world!");
+		Document myDoc = collection.find().first();
+
+		out.println(myDoc.toJson());
+		
 		out.close();
+		
+		mongoClient.close();
 	}
 }
